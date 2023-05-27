18b15eabd29f1cd355e63022d6b6e38bc23de6f8
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-05-27 05:05:03 +0300

    Update 01. Exchange Integers.py

--- 2.2 Data Types and Variables - More Exercises/01. Exchange Integers.py
+++ 2.2 Data Types and Variables - More Exercises/01. Exchange Integers.py
@@ -1,3 +1,7 @@
+a, b = input(), input()
+print(f'Before:\na = {a}\nb = {b}\nAfter:\na = {b}\nb = {a}')
+
+#-----------------------------------------(2)-----------------------
 a, b = [int(input()) for _ in range(2)]
 print(f'Before:\na = {a}\nb = {b}')
 print(f'After:\na = {b}\nb = {a}')
